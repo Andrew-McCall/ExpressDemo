83fdcb6c17e27d7a18b8a60048caeda9a9d277a8
Andrew-McCall <amccall@qa.com>
2022-09-30 12:05:55 +0100

    /post

--- tests/Express.js
+++ tests/Express.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const server = require("../index")
@@ -22,6 +23,21 @@ describe("API", function(){
             chai.expect(res).to.have.property("text")
             chai.expect(res.text).to.equal("Hello World!")
             
+        })
+    })
+
+    it("/post", function(){
+        const body = {test:'test Data'}
+        chai.request(url+"/api").post("/post").send(body).end((err, res) => {
+
+            chai.expect(err).to.be.null;
+            chai.expect(res).to.have.status(201)
+
+            chai.expect(res.body).to.have.property("test")
+            chai.expect(res.body.test).to.equal(body.test);
+
+            chai.expect(JSON.stringify(res.body)).to.equal(JSON.stringify(body))
+
         })
     })
 
